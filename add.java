65ac227addf2f426798960225d030be19d218c21
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2023-08-27 11:16:45 +0530

    expalin

--- add.java
+++ add.java
@@ -1,5 +1,23 @@
 package JavaProgram;
 
+import java.util.Scanner;
+
 public class add {
+  public static void main(String[] args) {
     
+      
+    Scanner sc=new Scanner(System.in);
+    int a;
+    int b;
+
+    a=sc.nextInt();
+    b=sc.nextInt();
+
+    System.out.println(a);
+    System.out.println(b);
+    sc.close();
+
+  }
+
+
 }
