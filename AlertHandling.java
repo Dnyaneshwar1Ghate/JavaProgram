25ccf547165514308f98b80e03f8a443f1821014
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2023-08-27 17:08:45 +0530

    chages done

---
+++ AlertHandling.java
@@ -0,0 +1,35 @@
+ package JavaProgram;
+
+//import java.util.concurrent.TimeUnit;
+
+
+public class AlertHandling {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver","F:\\Software\\driver\\chromedriver_win32\\chromedriver.exe");
+		//WebDriver driver =new ChromeDriver();
+			
+		//driver.manage().window().maximize();
+
+		//driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		//driver.get("https:demoqa.com/alerts");
+		Thread.sleep(3000);
+		
+		//driver.findElement(By.id("conformButton")).click();
+		Thread.sleep(3000);
+		
+		//System.out.println(driver.switchTo().alert().getText());
+		//String expResult="Do you want to action";
+		
+	
+		
+		
+		
+		
+			
+
+	}
+
+}
--- Amazon_Auto.java
+++ Amazon_Auto.java
@@ -1,11 +1,6 @@
-package SeleniumT;
+package JavaProgram;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import java.util.concurrent.TimeUnit;
 
 public class Amazon_Auto {
 
@@ -14,13 +9,13 @@ public class Amazon_Auto {
 		/*System.setProperty("webdriver.chrome.driver", "F:\\\\Software\\\\driver\\\\chromedriver_win32\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();*/
 		System.setProperty("webdriver.gecko.driver", "F:\\Software\\geckodriver-v0.32.2-win32\\geckodriver.exe");
-		WebDriver driver =new FirefoxDriver();
+		//WebDriver driver =new FirefoxDriver();
 		
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
        
        // implicit 
-       driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-       driver.get("https://www.amazon.in/");
+       //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+     /*  driver.get("https://www.amazon.in/");
        driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
       Thread.sleep(3000);
       driver.findElement(By.id("ap_email")).sendKeys("dghate2013@gmail.com");
@@ -31,6 +26,6 @@ public class Amazon_Auto {
       //auth-signin-button
       driver.findElement(By.id("auth-signin-button")).click();
       
-       
+       */
 	}
 }
